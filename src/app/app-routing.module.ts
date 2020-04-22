8833067756c83f72d0a5afb8e1cd2e924eee95ef
hardik <hpatel5@slb.com>
2020-04-22 15:52:03 +0530

    removed resolver

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { RouterModule, Routes } from "@angular/router";
 
-import { CourseResolver } from "./course/course.resolver";
 import { CoursesListComponent } from "./course/component/courses-list/courses-list.component";
 import { CreateCourseComponent } from "./course/component/create-course/create-course.component";
 import { NgModule } from "@angular/core";
@@ -8,10 +7,7 @@ import { NgModule } from "@angular/core";
 const routes = [
   {
     path: "courses",
-    component: CoursesListComponent,
-    resolve: {
-      courses: CourseResolver
-    }
+    component: CoursesListComponent
   },
   { path: "create-course", component: CreateCourseComponent },
   { path: "**", redirectTo: "courses" }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,13 @@ import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { BrowserModule } from "@angular/platform-browser";
 import { CourseModule } from "./course/course.module";
-import { CourseResolver } from "./course/course.resolver";
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, CourseModule],
-  providers: [CourseResolver],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
