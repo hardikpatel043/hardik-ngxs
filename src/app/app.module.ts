bf7a670c405c81ede1f3c80462f0fd6983feecc9
hardik <hpatel5@slb.com>
2020-04-22 15:51:18 +0530

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { AppRoutingModule } from "./app-routing.module";
 import { BrowserModule } from "@angular/platform-browser";
 import { CourseModule } from "./course/course.module";
 import { CourseResolver } from "./course/course.resolver";
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CourseModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, CourseModule],
   providers: [CourseResolver],
   bootstrap: [AppComponent]
 })
